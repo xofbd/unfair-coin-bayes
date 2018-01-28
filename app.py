8b1ac6519beb381fffd3297c0280365163c2b2e8
xofbd <dfox09@gmail.com>
2018-01-28 17:22:21 -0500

    add user input

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 from bokeh_plot import create_plot
 
 app = Flask(__name__)
+app.vars = {}
 
 
 @app.route('/')
 def main():
-    return redirect('/plot')
+    return redirect('/index')
 
 
-@app.route('/plot')
+@app.route('/index', methods=['GET'])
 def index():
-    script, div = create_plot()
-    return render_template('plot.html', script=script, div=div)
+    return render_template('index.html')
+
+
+@app.route('/plot', methods=['POST'])
+def plot():
+    app.vars['probability'] = float(request.form['probability'])
+    script, div = create_plot(Pi=app.vars['probability'])
+    return render_template('plot.html', Pi=app.vars['probability'], script=script, div=div)
 
 if __name__ == '__main__':
     app.run(port=33507, debug=True)
