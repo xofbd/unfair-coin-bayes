7714c252abc893a2b09522ecfe787930bc0cb22e
xofbd <dfox09@gmail.com>
2019-01-29 16:57:12 -0500

    use local rather than global variable

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, redirect, render_template, request
 from prob_dist import create_plot
 
 app = Flask(__name__)
-app.vars = {}
 
 
 @app.route('/')
@@ -17,19 +16,20 @@ def index():
 
 @app.route('/plot', methods=['POST'])
 def plot():
-    app.vars['probability'] = float(request.form['probability'])
-    app.vars['prior'] = request.form['prior']
+    params = {}
+    params['probability'] = float(request.form['probability'])
+    params['prior'] = request.form['prior']
 
     # define distribution parameters based on chosen prior
-    if app.vars['prior'] == 'uniform':
-        app.vars['a_prior'] = 1
-        app.vars['b_prior'] = 1
+    if params['prior'] == 'uniform':
+        params['a_prior'] = 1
+        params['b_prior'] = 1
     else:
-        app.vars['a_prior'] = int(request.form['a'])
-        app.vars['b_prior'] = int(request.form['b'])
+        params['a_prior'] = int(request.form['a'])
+        params['b_prior'] = int(request.form['b'])
 
-    script, div = create_plot(app.vars['probability'], app.vars['a_prior'],
-                              app.vars['b_prior'])
+    script, div = create_plot(params['probability'], params['a_prior'],
+                              params['b_prior'])
 
     return render_template('plot.html', script=script, div=div)
 
