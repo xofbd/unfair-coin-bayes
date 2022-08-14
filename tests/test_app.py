76cc79328dd64c27e5708455f9ffcc8afa763215
xofbd <dfox09@gmail.com>
2022-08-14 18:32:59 -0400

    Implement the POST-REDIRECT-GET pattern
    This pattern lets one use the browser's back button.

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,6 +6,8 @@ from app.app import app
 @pytest.fixture
 def test_client():
     with app.test_client() as test_client:
+        app.config['WTF_CSRF_ENABLED'] = False
+
         return test_client
 
 
@@ -28,7 +30,7 @@ def test_plot(test_client):
     THEN request is satisfied with the expected data
     """
     data = {'probability': 0.5, 'prior': 'Uniform', 'param_a': 1, 'param_b': 1}
-    response = test_client.post('/plot', data=data)
+    response = test_client.post('/', data=data)
 
     assert response.status_code == 200
     assert b"Coin flip simulation" in response.data
