ad00ba306a581e5a8482d26326326f988c34372d
xofbd <dfox09@gmail.com>
2022-09-09 17:23:29 -0400

    Better apply server side validation & formatting
    - Update bootstrap version
    - Use red letters when invalid data is passed
    - Use Greek letters for beta parameters
    - Refactor out macros to separate template file
    - Add Beautiful Soup to dev requirements
    - Update tests and use Beautiful Soup

--- app/forms.py
+++ app/forms.py
@@ -4,18 +4,26 @@ from wtforms.validators import InputRequired, NumberRange
 
 
 class ProbabilityForm(FlaskForm):
-    probability = FloatField('Probability (0 to 1):',
-                             render_kw={'placeholder': 0.5},
-                             validators=[InputRequired(), NumberRange(min=0, max=1)])
-    prior = RadioField('Choose Prior',
-                       default='Uniform',
-                       choices=['Uniform', 'Beta'],)
-    param_a = IntegerField('a',
-                           default=1,
-                           render_kw={'placeholder': 2},
-                           validators=[InputRequired(), NumberRange(min=0)])
-    param_b = IntegerField('b',
-                           default=1,
-                           render_kw={'placeholder': 2},
-                           validators=[InputRequired(), NumberRange(min=0)])
+    probability = FloatField(
+        'Probability of heads',
+        render_kw={'placeholder': 0.5},
+        validators=[InputRequired(), NumberRange(min=0, max=1)]
+    )
+    prior = RadioField(
+        'Choose Prior',
+        default='Uniform',
+        choices=['Uniform', 'Beta']
+    )
+    param_a = IntegerField(
+        'Shape parameter α',
+        default=1,
+        render_kw={'placeholder': 2},
+        validators=[InputRequired(), NumberRange(min=0)]
+    )
+    param_b = IntegerField(
+        'Shape parameter β',
+        default=1,
+        render_kw={'placeholder': 2},
+        validators=[InputRequired(), NumberRange(min=0)]
+    )
     submit = SubmitField('Submit')
